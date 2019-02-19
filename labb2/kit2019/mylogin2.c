682c63d10ad8964fed3246308648ac66035991d5
Trolliver <trolliver1337@hotmail.se>
2019-02-19 17:48:39 +0100

    Update mylogin2.c

--- labb2/kit2019/mylogin2.c
+++ labb2/kit2019/mylogin2.c
@@ -1,1 +1,70 @@
+/*
+ * Shows user info from local pwfile.
+ *  
+ * Usage: userinfo username
+ */
+
+#define _XOPEN_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <crypt.h>
+#include "pwdblib.h"   /* include header declarations for pwdblib.c */
+
+/* Define some constants. */
+#define USERNAME_SIZE (32)
+#define NOUSER (-1)
+
+int login_user(const char *username)
+{
+    struct pwdb_passwd *p = pwdb_getpwnam(username);
+    if (p != NULL) {
+        char *password;
+        password = getpass("password: ");
+
+        // copies the 2 first elements to salt(as it is the salt).
+        char salt[2]; 
+        memcpy(salt, p->pw_passwd, 2); 
+
+        char *pwhash;
+        //encrypts the plaintext password with the salt.
+        pwhash = crypt(password, salt); 
+
+        // Is the stored hash the same as the calculated one?
+        if (strcmp(p->pw_passwd, pwhash) == 0) {
+            printf("\nDucking success mate\n");
+        } else {
+            printf("\nDucking fail mate\n");
+        }
+        return 0;
+    } else {
+        return NOUSER;
+    }
+}
+
+
+
+void read_username(char *username)
+{
+    printf("login: ");
+    fgets(username, USERNAME_SIZE, stdin);
+
+    /* remove the newline included by getline() */
+    username[strlen(username) - 1] = '\0';
+}
+
+int main(int argc, char **argv)
+{
+    char username[USERNAME_SIZE];
+    /* 
+     * Write "login: " and read user input. Copies the username to the
+     * username variable.
+     */
+    read_username(username);
+
+    if (login_user(username) == NOUSER) {
+        printf("\nFound no user with name: %s\n", username); 
+        return 0;
+    }
+}
 
